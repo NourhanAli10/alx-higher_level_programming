dede27a718afaf98f37c8781cea107de76635c45
NourhanAR <nouranonamm@gmail.com>
2023-10-03 21:55:06 +0300

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(str) <= ord('A') and ord(str) >= ord('Z'):
-        return True
-    else:
-        return False
+    for i in str:
+        if ord(i) >= ord('A') and ord(i) <= ord('Z'):
+            print("{}".format(chr(i)))
+
