f03e9c6b6d2131a4d419694b7d7692e6f60ef7e7
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-10-03 07:58:45 -0700

    task 6,7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i+1, 10):
         if j == 10 - 1 and i == 10 -2:
-            print("{}{}".format(i, j), end = " ")
+            print("{}{}".format(i, j))
         else:
             print("{}{},".format(i, j), end = " ")
