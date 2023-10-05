b9b420c1f0d2dadede13a91acd7f4a1c3b241e7b
root <root@DESKTOP-NH1UJLS.localdomain>
2023-10-06 00:16:36 +0300

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
+    files = dir(hidden_4)
+    for file in files:
+        if file[:2] != '__' and file[-1:-3] != '__':
+            print("{}".format(file))
     
     
     
