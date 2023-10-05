24ed3cb3031bfa44b5c90dc4a8beedc0d1c0b572
root <root@DESKTOP-NH1UJLS.localdomain>
2023-10-05 17:41:34 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,9 @@ if __name__ == "__main__":
     if number == 1:
         print("{} arguments.".format(number - 1))
     elif number == 2:
-        print("{} argument:".format(number - 1))  
-        print("{}: {}".format(number - 1, sys.argv[number -1])) 
+        print("{} argument:".format(number - 1))
+        print("{}: {}".format(number - 1, sys.argv[number - 1]))
     else:
-        print("{} arguments:".format(number - 1)) 
-        for i in range(1, number):                  
-            print("{}: {}".format(i, sys.argv[i])) 
-            
-
- 
+        print("{} arguments:".format(number - 1))
+        for i in range(1, number):                      
+            print("{}: {}".format(i, sys.argv[i]))            
