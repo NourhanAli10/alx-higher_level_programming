1e8e9aeb2ce30c6f7b08ced3a204ac36b718c4d8
root <root@DESKTOP-NH1UJLS.localdomain>
2023-10-06 01:02:51 +0300

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
-input()
-operators = ()
+import sys
+import calculator_1 as cal
+length = len(sys.argv)
+if length != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+ 
+operator = sys.argv[2] 
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+if length == 4:
+    if operator == '+':
+        print("{} {} {} = {}".format(a, operator, b, cal.add(a, b)))
+    elif operator == '-':
+        print("{} {} {} = {}".format(a, operator, b, cal.sub(a, b)))
+    elif operator == '*':
+        print("{} {} {} = {}".format(a, operator, b, cal.mul(a, b)))
+    elif operator == '/':
+        print("{} {} {} = {}".format(a, operator, b, cal.div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+        
+    
