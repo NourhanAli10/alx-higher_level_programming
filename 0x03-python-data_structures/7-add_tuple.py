4a223366c88b8f6a79a879e12e091e5b82e3981f
root <root@DESKTOP-NH1UJLS.localdomain>
2023-10-07 17:03:23 +0300

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,13 +3,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) < 2 or len(tuple_b) == 0:
         for i in range(len(tuple_b), 2):
             tuple_b += (0,)
-    elif len(tuple_a) < 2 or len(tuple_a) == 0:
+    elif  len(tuple_b) > 2:
+        tuple_b = (tuple_b[0], tuple_b[1])        
+    if len(tuple_a) < 2 or len(tuple_a) == 0:
         for i in range(len(tuple_a), 2):
             tuple_a += (0,)
     elif  len(tuple_a) > 2:
         tuple_a = (tuple_a[0], tuple_a[1])
-    elif  len(tuple_b) > 2:
-        tuple_b = (tuple_b[0], tuple_b[1])
+    
 
     new_tuple = ()
     for a, b in zip(tuple_a, tuple_b):
