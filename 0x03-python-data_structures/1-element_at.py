bc9957c6bf7cb486e59d9e5a7a942890a9327880
root <root@DESKTOP-NH1UJLS.localdomain>
2023-10-07 13:03:40 +0300

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    element = my_list.pop(idx)
+    if idx < 0:
+        element = "None"
+    elif idx > len(my_list) - 1:
+        element = "None"
+    else:
+        element = my_list.pop(idx)
     return (element)
