5c60c04ca742372fd332855190d09083da8d0bac
root <root@DESKTOP-NH1UJLS.localdomain>
2023-10-12 00:31:25 +0300

    task 2,3

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    eleindex = new_list.index(search)
-    new_list[eleindex] = replace
+    for i in my_list:
+        eleindex = new_list.index(search)
+        new_list[eleindex] = replace
     return new_list
