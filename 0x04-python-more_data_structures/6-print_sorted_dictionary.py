2c4efc50dfbff1b2d56f25b96b90a065fa64558d
root <root@DESKTOP-NH1UJLS.localdomain>
2023-10-12 04:30:55 +0300

    tasks

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_dict = dict(sorted(a_dictionary.items()))
+    for item in sorted_dict:
+        print("{}: {}".format(item, sorted_dict.get(item)))    
---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    add = {key: value}
+    a_dictionary.update(add)
