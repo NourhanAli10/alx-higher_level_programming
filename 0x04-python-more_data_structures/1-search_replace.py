ce3c92b7e6fe773b29934c8fc48afc447a3f33a7
root <root@DESKTOP-NH1UJLS.localdomain>
2023-10-12 05:13:26 +0300

    tasks

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for i in my_list:
-        eleindex = new_list.index(search)
-        new_list[eleindex] = replace
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i]= replace
     return new_list
