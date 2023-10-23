56c89a77ae78eb35ef8f7331e92eb1b6a289864c
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-10-24 01:26:38 +0300

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,7 @@ def safe_print_integer(value):
     try:
         if isinstance(value, int):
             print("{:d}".format(value))
+            print()
             return True
     except ValueError:
         return False
