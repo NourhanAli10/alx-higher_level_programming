d57657f867e9fb05893c8724941d6a6b9c172b06
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-10-25 01:56:40 +0300

    tasks

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 class Square:
+    """Defines a square"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """Initialises the data"""
+        self.__size = size
+        if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        return self.__size ** 2
