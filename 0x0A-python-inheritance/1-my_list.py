63b80c7a0eda3155e884f8f583b85898f51091a6
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-11-07 00:47:46 +0200

    task 2 ,3

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module provides a single function 'lookup' that
+can be used to retrieve a list of attributes and methods
+of any Python object.
+"""
+
+class MyList(list):
+    
+    def print_sorted(self):
+        sorted_list = sorted(list)
+        return sorted_list
