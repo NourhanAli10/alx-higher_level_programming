ffd1c173208a5b094c154ef6cd07e55af6116c5c
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-11-16 01:40:42 +0200

    tasks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,3 +21,19 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+        
+        
+    def update(self, *args, **kwargs):
+        """function to update the class Square"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
