3ad00f6238c33a9223ef412b9dde1b3b5b0548ac
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-11-15 02:36:08 +0200

    tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,10 +19,6 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -31,10 +27,6 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -43,10 +35,6 @@ class Rectangle(Base):
 
     @x.setter
     def set_x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -55,17 +43,4 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
         self.__y = value
-
-    def area(self):
-        return self.__width * self.__height
-
-    def display(self):
-        for x in range(self.__height):
-            for i in range(self.__width):
-                print("#", end="")
-            print(" ")
