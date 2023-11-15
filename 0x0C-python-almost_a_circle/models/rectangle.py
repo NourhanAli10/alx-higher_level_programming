9112e242ebacc45690a54adf7f2e953ae92c6cd7
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-11-15 02:34:32 +0200

    tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
+""" this module is about class named base"""
 from models.base import Base
 
 
 class Rectangle(Base):
+    """class Rectangle that ingerit from base"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """this function to define instance attribute """
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        
-        
-    def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
-        
 
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -26,10 +24,11 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -37,10 +36,11 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
+
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def set_x(self, value):
         if not isinstance(value, int):
@@ -48,11 +48,11 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
-    
+
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         if not isinstance(value, int):
@@ -60,15 +60,12 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
-        
-        
 
     def area(self):
         return self.__width * self.__height
-    
+
     def display(self):
         for x in range(self.__height):
             for i in range(self.__width):
                 print("#", end="")
             print(" ")
-            
