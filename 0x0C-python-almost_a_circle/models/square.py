2448c200d551e41f43364aa5d1eff132bcf801c1
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-11-16 01:20:59 +0200

    tasks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,9 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """this class name square"""
     def __init__(self, size, x=0, y=0, id=None):
+        """this function to define instance attribute """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """this function to it returns [Square] (<id>) <x>/<y> - <size>"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
