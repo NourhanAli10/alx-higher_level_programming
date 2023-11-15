d2ea89a8d191d29934aa262f4dfbf9707853ffcb
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-11-16 01:33:36 +0200

    tasks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,12 @@ class Square(Rectangle):
     def __str__(self):
         """this function to it returns [Square] (<id>) <x>/<y> - <size>"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
