ee8d65c71d2af90db39128f03faa6f1f4691120a
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-12-14 00:29:24 +0200

    tasks

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,9 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `cities` (
-    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cities`(
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES states(id)
+    FOREIGN KEY(state_id) REFERENCES states(id),    
+    name VARCHAR(256) NOT NULL
 );
