0ef1d6b3cd2c24dd6ee9e595fc4ac68314dea9af
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-10 01:14:25 +0200

    tasks

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,24 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.error('Usage: node 102-concat.js <file1> <file2> <destination>');
-  process.exit(1);
-}
+const fs = require('fs').promises;
+const { argv } = require('process');
 
-const file1Path = process.argv[2];
-const file2Path = process.argv[3];
-const destinationPath = process.argv[4];
+fs.readFile(argv[2], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, 'utf8'))
+  .catch(err => console.error(err));
 
-try {
-  const content1 = fs.readFileSync(file1Path, 'utf-8');
-  const content2 = fs.readFileSync(file2Path, 'utf-8');
-
-  const concatenatedContent = content1 + content2;
-
-  fs.writeFileSync(destinationPath, concatenatedContent);
-
-  console.log('Files concatenated successfully!');
-} catch (err) {
-  console.error('Error:', err.message);
-}
+fs.readFile(argv[3], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
+  .catch(err => console.error(err));
