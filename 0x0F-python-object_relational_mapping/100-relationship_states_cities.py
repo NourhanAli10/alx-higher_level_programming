884f9b048929c33501c37e28e52c204172f5661d
NourhanAR <nouranonamm@gmail.com>
2024-01-15 03:41:40 +0200

    tasks

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""
-Script that creates the State “California” with the City “San Francisco”
-from the database hbtn_0e_100_usa
-"""
-
+# Creates the State “California” with the City “San Francisco”
+# from the database hbtn_0e_100_usa.
+# Usage: ./100-relationship_states_cities.py <mysql username> /
+#                                            <mysql password> /
+#                                            <database name>
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
+from relationship_state import State
+from relationship_city import Base, City
 
 if __name__ == "__main__":
-    # Check if correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Connect to MySQL server running on localhost at port 3306
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        username, password, database), pool_pre_ping=True)
-
-    # Create all tables in the engine
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    # Create a session
     Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(City(name="San Francisco", state=State(name="California")))
+    session.commit()
