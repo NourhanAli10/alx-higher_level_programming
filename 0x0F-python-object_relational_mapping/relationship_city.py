c8c4ff239faad80bb0e217cfce958e592131e326
NourhanAR <nouranonamm@gmail.com>
2024-01-15 03:46:47 +0200

    tasks

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+from sqlalchemy import Column, Integer, String, text, ForeignKey
+from relationship_state import Base
 """
-City Module
+    Module that performs creates a States class based off of Base.
 """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from relationship_state import Base, State
 
 class City(Base):
-    """City class"""
+    """
+        The ``City`` class which inherits from ``Base`` class.
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship('State', back_populates='cities')
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""
-State module
-"""
-
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, text
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+"""
+    Module that performs creates a States class based off of Base.
+"""
 
 Base = declarative_base()
 
+
 class State(Base):
-    """State class"""
+    """
+        The ``States`` class which inherits from ``Base`` class.
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete-orphan', back_populates='state')
+
+    cities = relationship("City", backref="state", cascade="all, delete")
