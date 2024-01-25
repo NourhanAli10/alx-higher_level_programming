76b674b35d5580fa4f7412931bd9e3fe7bc9fc90
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-26 00:05:20 +0200

    tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
+curl -sI "$url" | grep -i content-length | awk '{print $2}' | tr -d '\r'
+
