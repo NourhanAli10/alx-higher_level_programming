963d15cdcf8956de4d3eb99e12bdfa1322b9befa
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-28 00:32:54 +0200

    tasks

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,9 +8,9 @@ import sys
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     if len(sys.argv) > 1:
-        letter = sys.argv[1]    
+        letter = sys.argv[1]
     else:
-        lertter = ""
+        letter = ""
     params = {'q': letter}
     try:
         response = requests.post(url, data=params)
