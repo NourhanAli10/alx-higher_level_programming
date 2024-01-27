a9e73a3882e9e6deebdd7e89b6af3c6020670655
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-27 23:58:15 +0200

    tasks

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,7 @@ import sys
 if __name__ == "__main__":
     Url = sys.argv[1]
     response = requests.get(Url)
-    print(response.text)
     if response.status_code >= 400:
         print("Error code:", response.status_code)
+    else:
+        print(response.text)
