2efebec12b1628d81efd4b7965aa794537f0cd63
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-27 23:40:54 +0200

    tasks

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     params = {'email': email}
-    response = requests.post(url, params)
+    response = requests.get(url, params)
     print(response.text)
