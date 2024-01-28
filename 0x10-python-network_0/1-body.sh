6f3782740b1980587bf502c0550633356583f2cb
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-28 02:07:03 +0200

    tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sI {$1}
+# Bash script that takes in a URL, sends a GET request to the URL,
+# and displays the body of the response
+curl -sL {$1}
