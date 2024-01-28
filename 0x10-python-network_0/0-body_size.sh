8c3a591784c1f1aa6fb9849d1efacc42accf68b0
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-28 17:09:05 +0200

    tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# Bash script that takes in a URL, sends a request to that URl
+curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r\n'
