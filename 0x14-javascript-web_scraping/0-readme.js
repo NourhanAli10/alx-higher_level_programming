e9dea9ebf423f1b812ca7701166c36bbd6681fc8
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-21 00:33:26 +0200

    tasks

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+console.log(filePath);
+
+fs.readFile(filePath, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.stringify(data));
+  }
+});
